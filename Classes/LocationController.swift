4a55508e165583b5f95bbbe0e7be74014b59173c
Tejas Thanki <tejasdthanki@yahoo.com>
2021-02-13 14:59:16 +0530

    added logic for get user's current location

--- Classes/LocationController.swift
+++ Classes/LocationController.swift
@@ -2,7 +2,91 @@
 //  LocationController.swift
 //  GetLocation
 //
-//  Created by Tejas Thanki on 13/02/21.
+//  Created by Tejas Thanki on 11/02/21.
 //
 
-import Foundation
+import UIKit
+import CoreLocation
+public typealias locationHandler  = (_:String?,_:CLLocation?) -> Void
+
+public class LocationController: NSObject,CLLocationManagerDelegate {
+    static let shared:LocationController = LocationController()
+    public var locationHandlercallBack:locationHandler?
+    var clLocationManager = CLLocationManager()
+    var locationServiceMsg:String = "Location service is disable in your device.Please enable it in settings."
+    var locationPermissionMsg:String = "Location permission is disable in your device.Please allow it from setting manually."
+    
+    public func getUserCurrentLocation(callBack:@escaping locationHandler){
+        locationHandlercallBack = callBack
+        if CLLocationManager.locationServicesEnabled() == true {
+            if CLLocationManager.authorizationStatus() == .restricted || CLLocationManager.authorizationStatus() == .denied || CLLocationManager.authorizationStatus() == .notDetermined {
+                clLocationManager.requestWhenInUseAuthorization()
+            }
+            clLocationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            clLocationManager.delegate = self
+            clLocationManager.distanceFilter = kCLLocationAccuracyBest
+        }else{
+            locationHandlercallBack!(locationServiceMsg,nil)
+        }
+    }
+
+    public func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            // If status has not yet been determied, ask for authorization
+            manager.requestWhenInUseAuthorization()
+            break
+        case .authorizedWhenInUse,.authorizedAlways:
+            self.clLocationManager.startUpdatingLocation()
+            break
+        case .restricted,.denied:
+            // If user denied your app access to Location Services, but can grant access from Settings.app
+            locationHandlercallBack!(locationPermissionMsg,nil)
+            break
+        default:
+            break
+        }
+    }
+    
+    public func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        if #available(iOS 14.0, *) {
+            let status = manager.authorizationStatus
+            switch status {
+            case .notDetermined:
+                // If status has not yet been determied, ask for authorization
+                manager.requestWhenInUseAuthorization()
+            break
+            case .authorizedWhenInUse,.authorizedAlways:
+                let accuracyStatus = manager.accuracyAuthorization
+                if accuracyStatus == CLAccuracyAuthorization.reducedAccuracy{
+                    self.clLocationManager.requestTemporaryFullAccuracyAuthorization(withPurposeKey: "wantAccurateLocation", completion: { [self]
+                        error in
+                        self.clLocationManager.startUpdatingLocation()
+                    })
+                }else{
+                    self.clLocationManager.startUpdatingLocation()
+                }
+                break
+            case .restricted,.denied:
+                // If user denied your app access to Location Services, but can grant access from Settings.app
+                locationHandlercallBack!(locationPermissionMsg,nil)
+            break
+            default:
+                break
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+    
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if(locationHandlercallBack != nil){
+            locationHandlercallBack!(nil,locations[0])
+        }
+        locationHandlercallBack = nil
+    }
+    
+    public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        locationHandlercallBack!(error.localizedDescription,nil)
+    }
+}
--- Example/GetLocation/ViewController.swift
+++ Example/GetLocation/ViewController.swift
@@ -2,23 +2,31 @@
 //  ViewController.swift
 //  GetLocation
 //
-//  Created by tejasdthanki on 02/13/2021.
-//  Copyright (c) 2021 tejasdthanki. All rights reserved.
+//  Created by Tejas Thanki on 11/02/21.
 //
 
 import UIKit
-
+import CoreLocation
+import GetLocation
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblLocation: UILabel!
+    let manager = LocationController()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupLocation()
+        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    @objc func setupLocation(){
+        manager.getUserCurrentLocation { [self] (error, location) in
+            if(error != nil){
+                lblLocation.text = error
+            }else{
+                lblLocation.text = ["lat : ",String(format: "%.4f",location!.coordinate.latitude),", long: ",String(format: "%.4f",location!.coordinate.longitude)].joined()
+            }
+        }
+    }
 }
 
