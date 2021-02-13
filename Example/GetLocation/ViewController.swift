f9e198f87b9b9a8d69999d5a8c6fefd0166793d0
Tejas Thanki <tejasdthanki@yahoo.com>
2021-02-13 14:45:51 +0530

    Initial commit

---
+++ Example/GetLocation/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GetLocation
+//
+//  Created by tejasdthanki on 02/13/2021.
+//  Copyright (c) 2021 tejasdthanki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
